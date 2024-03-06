36c2d4ca7149da71ed3b69b5da2ce1c29fe9f9b7
anonymousTTS <amit.roth.43@gmail.com>
2024-03-06 22:56:37 +0200

    added samples

--- render_batch.py
+++ render_batch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Generate forms for human evaluation."""
+import json
 from pathlib import Path
 
 from jinja2 import FileSystemLoader, Environment
@@ -12,148 +13,184 @@ def main():
     template = env.get_template("batch_comparison.html.jinja2")
 
     # names should be unique and with correct format for app.gs
+    with open("samples/samples.json", 'r') as f:
+        samples_json = json.load(f)
+
+    directories = [
+        'mms',
+        'roboshaul',
+        'word_osim',
+        'word_roboshaul'
+    ]
+
     questions = [
         {
-            "prompt": "ובשביל להבין למה מחיר הדלק כל כך עלה, צריך לחזור שנתיים אחרונית.",
+            "id": i,
+            "prompt": s['prompt'],
             "recordings": [
                 {
-                    "title": f"mms",
-                    "audio_path": f"samples/mms/mms_2.wav",
-                    "name": f"mms1"
-                },
-                {
-                    "title": f"robo",
-                    "audio_path": f"samples/roboshaul/robo_2.wav",
-                    "name": f"robo1"
-                },
-                {
-                    "title": f"valle robo",
-                    "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_1_2.wav",
-                    "name": f"v-robo1"
-                },
-                {
-                    "title": f"valle osim",
-                    "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_1_2.wav",
-                    "name": f"v-osim1"
-                }
+                    'title': d,
+                    'audio_path': f"samples/{d}/{s['index']}.wav",
+                    'name': f"{d}_{s['index']}"
+                } for d in directories
             ]
-        },
-
-        {
-            "prompt": "תגידו, גנבו לכם פעם את האוטו ופשוט ידעתם שאין טעם להגיש תלונה במשטרה",
-            "recordings": [
-                {
-                    "title": f"mms",
-                    "audio_path": f"samples/mms/mms_4.wav",
-                    "name": f"mms2"
-                },
-                {
-                    "title": f"robo",
-                    "audio_path": f"samples/roboshaul/robo_4.wav",
-                    "name": f"robo2"
-                },
-                {
-                    "title": f"valle robo",
-                    "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_3_0.wav",
-                    "name": f"v-robo2"
-                },
-                {
-                    "title": f"valle osim",
-                    "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_3_2.wav",
-                    "name": f"v-osim2"
-                }
-            ]
-        },
-
-        {
-            "prompt": "מה שבהגדרה משאיר את הכלכלה ההונגרית מאחור, אפילו ביחס למדינות כמו פולין",
-            "recordings": [
-                {
-                    "title": f"mms",
-                    "audio_path": f"samples/mms/mms_5.wav",
-                    "name": f"mms3"
-                },
-                {
-                    "title": f"robo",
-                    "audio_path": f"samples/roboshaul/robo_5.wav",
-                    "name": f"robo3"
-                },
-                {
-                    "title": f"valle robo",
-                    "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_4_1.wav",
-                    "name": f"v-robo3"
-                },
-                {
-                    "title": f"valle osim",
-                    "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_4_0.wav",
-                    "name": f"v-osim3"
-                }
-            ]
-        },
-
-        {
-            "prompt": "הדרבי תמיד היה המשחק הכי חשוב, אך בשנים האחרונות הוא נעשה כמעט הדבר היחיד שחשוב",
-            "recordings": [
-                {
-                    "title": f"mms",
-                    "audio_path": f"samples/mms/mms_6.wav",
-                    "name": f"mms4"
-                },
-                {
-                    "title": f"robo",
-                    "audio_path": f"samples/roboshaul/robo_6.wav",
-                    "name": f"robo4"
-                },
-                {
-                    "title": f"valle robo",
-                    "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_5_0.wav",
-                    "name": f"v-robo4"
-                },
-                {
-                    "title": f"valle osim",
-                    "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_5_0.wav",
-                    "name": f"v-osim4"
-                }
-            ]
-        },
-
-        {
-            "prompt": "כשני שלישים מייצור הסוללות העולמי מתרחשים בסין לעומת עשרה אחוזים בארצות הברית",
-            "recordings": [
-                {
-                    "title": f"mms",
-                    "audio_path": f"samples/mms/mms_10.wav",
-                    "name": f"mms5"
-                },
-                {
-                    "title": f"robo",
-                    "audio_path": f"samples/roboshaul/robo_10.wav",
-                    "name": f"robo5"
-                },
-                {
-                    "title": f"valle robo",
-                    "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_9_2.wav",
-                    "name": f"v-robo5"
-                },
-                {
-                    "title": f"valle osim",
-                    "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_9_1.wav",
-                    "name": f"v-osim5"
-                }
-            ]
-        },
+        } for i, s in enumerate(samples_json)
     ]
+
+    print(questions)
+
 
 
     html = template.render(
         page_title="Hebrew TTS",
-        form_url="https://script.google.com/macros/s/AKfycbw1f06tdnuvXvye-dU59qvTOlp5UDR2AaB2vXBu_Rrtyqh849tD2nYJ9aMzhJYdrnW4iw/exec",
+        form_url="https://script.google.com/macros/s/AKfycbylNUBrNAY05AeTb65Cx9d6b4C7nIjAJl6hhtk3pPk2MOOl6yAqPWrJ2Un0XngVWGQb0Q/exec",
         form_id=1,
         questions=questions
     )
+
+    with open("index.html", 'w') as f:
+        f.write(html)
 
     print(html)
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+# questions = [
+    #     {
+    #         "prompt": "ובשביל להבין למה מחיר הדלק כל כך עלה, צריך לחזור שנתיים אחרונית.",
+    #         "recordings": [
+    #             {
+    #                 "title": f"mms",
+    #                 "audio_path": f"samples/mms/mms_2.wav",
+    #                 "name": f"mms1"
+    #             },
+    #             {
+    #                 "title": f"robo",
+    #                 "audio_path": f"samples/roboshaul/robo_2.wav",
+    #                 "name": f"robo1"
+    #             },
+    #             {
+    #                 "title": f"valle robo",
+    #                 "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_1_2.wav",
+    #                 "name": f"v-robo1"
+    #             },
+    #             {
+    #                 "title": f"valle osim",
+    #                 "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_1_2.wav",
+    #                 "name": f"v-osim1"
+    #             }
+    #         ]
+    #     },
+    #
+    #     {
+    #         "prompt": "תגידו, גנבו לכם פעם את האוטו ופשוט ידעתם שאין טעם להגיש תלונה במשטרה",
+    #         "recordings": [
+    #             {
+    #                 "title": f"mms",
+    #                 "audio_path": f"samples/mms/mms_4.wav",
+    #                 "name": f"mms2"
+    #             },
+    #             {
+    #                 "title": f"robo",
+    #                 "audio_path": f"samples/roboshaul/robo_4.wav",
+    #                 "name": f"robo2"
+    #             },
+    #             {
+    #                 "title": f"valle robo",
+    #                 "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_3_0.wav",
+    #                 "name": f"v-robo2"
+    #             },
+    #             {
+    #                 "title": f"valle osim",
+    #                 "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_3_2.wav",
+    #                 "name": f"v-osim2"
+    #             }
+    #         ]
+    #     },
+    #
+    #     {
+    #         "prompt": "מה שבהגדרה משאיר את הכלכלה ההונגרית מאחור, אפילו ביחס למדינות כמו פולין",
+    #         "recordings": [
+    #             {
+    #                 "title": f"mms",
+    #                 "audio_path": f"samples/mms/mms_5.wav",
+    #                 "name": f"mms3"
+    #             },
+    #             {
+    #                 "title": f"robo",
+    #                 "audio_path": f"samples/roboshaul/robo_5.wav",
+    #                 "name": f"robo3"
+    #             },
+    #             {
+    #                 "title": f"valle robo",
+    #                 "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_4_1.wav",
+    #                 "name": f"v-robo3"
+    #             },
+    #             {
+    #                 "title": f"valle osim",
+    #                 "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_4_0.wav",
+    #                 "name": f"v-osim3"
+    #             }
+    #         ]
+    #     },
+    #
+    #     {
+    #         "prompt": "הדרבי תמיד היה המשחק הכי חשוב, אך בשנים האחרונות הוא נעשה כמעט הדבר היחיד שחשוב",
+    #         "recordings": [
+    #             {
+    #                 "title": f"mms",
+    #                 "audio_path": f"samples/mms/mms_6.wav",
+    #                 "name": f"mms4"
+    #             },
+    #             {
+    #                 "title": f"robo",
+    #                 "audio_path": f"samples/roboshaul/robo_6.wav",
+    #                 "name": f"robo4"
+    #             },
+    #             {
+    #                 "title": f"valle robo",
+    #                 "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_5_0.wav",
+    #                 "name": f"v-robo4"
+    #             },
+    #             {
+    #                 "title": f"valle osim",
+    #                 "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_5_0.wav",
+    #                 "name": f"v-osim4"
+    #             }
+    #         ]
+    #     },
+    #
+    #     {
+    #         "prompt": "כשני שלישים מייצור הסוללות העולמי מתרחשים בסין לעומת עשרה אחוזים בארצות הברית",
+    #         "recordings": [
+    #             {
+    #                 "title": f"mms",
+    #                 "audio_path": f"samples/mms/mms_10.wav",
+    #                 "name": f"mms5"
+    #             },
+    #             {
+    #                 "title": f"robo",
+    #                 "audio_path": f"samples/roboshaul/robo_10.wav",
+    #                 "name": f"robo5"
+    #             },
+    #             {
+    #                 "title": f"valle robo",
+    #                 "audio_path": f"samples/word_roboshaul/roboshaul_roboshaul_top_50_temp_1_sample_9_2.wav",
+    #                 "name": f"v-robo5"
+    #             },
+    #             {
+    #                 "title": f"valle osim",
+    #                 "audio_path": f"samples/word_osim/osim_osim_top_50_temp_1_sample_9_1.wav",
+    #                 "name": f"v-osim5"
+    #             }
+    #         ]
+    #     },
+    # ]
